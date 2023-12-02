b66799a677a7f5a1930f76dc88cf7df5518cddae
18107 <18107@users.noreply.github.com>
2023-12-02 11:12:29 +1030

    Add files via upload

---
+++ Mod.cs
@@ -0,0 +1,22 @@
+﻿using PulsarModLoader;
+
+namespace LinuxMouseFix
+{
+    public class Mod : PulsarMod
+    {
+        public override string Version => "0.0.0";
+
+        public override string Author => "18107";
+
+        public override string ShortDescription => "Fixes the mouse movement on Linux";
+
+        public override string Name => "Linux Mouse Fix";
+
+        public override string ModID => "id107.linuxmousefix";
+
+        public override string HarmonyIdentifier()
+        {
+            return "id107.linuxmousefix";
+        }
+    }
+}
---
+++ Player.cs
@@ -0,0 +1,39 @@
+﻿using HarmonyLib;
+using UnityEngine;
+
+namespace LinuxMouseFix
+{
+    [HarmonyPatch(typeof(PLPlayer), "Update")]
+    internal class Player
+    {
+        static float verticalRotation = 0;
+        static float horizontalRotation = 0;
+        static bool[] lastButtonDown = new bool[] { false, false, false, false, false };
+        static bool[] buttonDown = new bool[] { false, false, false, false, false };
+        static bool buttonChanged = false;
+        static void Prefix(PLPlayer __instance, PLPawn ___MyPawn)
+        {
+            if (___MyPawn != null && ___MyPawn == PLNetworkManager.Instance.MyLocalPawn)
+            {
+                if (buttonChanged)
+                {
+                    ___MyPawn.VerticalMouseLook.RotationY = verticalRotation;
+                    ___MyPawn.HorizontalMouseLook.RotationX = horizontalRotation;
+                }
+                verticalRotation = ___MyPawn.VerticalMouseLook.RotationY;
+                horizontalRotation = ___MyPawn.HorizontalMouseLook.RotationX;
+
+                buttonChanged = false;
+                for (int i = 0; i < 5; i++)
+                {
+                    lastButtonDown[i] = buttonDown[i];
+                    buttonDown[i] = Input.GetMouseButton(i);
+                    if (buttonDown[i] != lastButtonDown[i])
+                    {
+                        buttonChanged = true;
+                    }
+                }
+            }
+        }
+    }
+}
